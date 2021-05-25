54bd2bd0f47a5322cf0cef172f6e521c2036dfa3
Nathany Mendonça <69328762+nathanymendonca@users.noreply.github.com>
2021-05-24 22:22:18 -0300

    uso de array

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -1,40 +1,36 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.Cliente;
-import br.com.bytebank.banco.modelo.Conta;
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
+public class Teste {
+	
+	
+	//Array [] tbm é um objeto
+	 public static void main(String[] args) {
 
-public class Teste { 
-
-  public static void main(String[] args) { 
-
-//    System.out.println("x");
-//    System.out.println(3);
-//    System.out.println(false);
-
-    Object cc = new ContaCorrente(22, 33);
-    Object cp = new ContaPoupanca(33, 22);
-    Object cliente = new Cliente();
-    
-        System.out.println(cc.toString());
-        System.out.println(cp.toString());
-
-        println(cliente);
-
+		 int[] idades = new int[5]; //inicializa o array com 0
+		 
+		 for(int i = 0; i < idades.length; i++) {
+			 idades[i] = i * i;
+		 }
+			 
+		 
+		 for (int i = 0; i < idades.length; i++) {
+			 System.out.println(idades[i]);
+		 }
+		 }
+		 
+		 
+//		 idades[0] = 29;
+//         idades[1] = 39;
+//         idades[2] = 49;
+//         idades[3] = 59;
+//         idades[4] = 69;
+//
+//         
+//         int idade1 = idades[2];
+//         
+//         
+//         System.out.println(idade1);
+//         
+//         System.out.println(idades.length); 
+//}
     }
-
-    static void println() {
-    }
-
-    static void println(int a) {
-    }
-
-    static void println(boolean valor) {
-    }
-
-    static void println(Object referencia) {
-    }
-
-} 
-
